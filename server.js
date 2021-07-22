11b06d083aba21764d8a3fb65dd0430ce94ef2a1
Falola Ibrahim A <ayofalola240@gmail.com>
2021-07-22 20:30:07 +0100

    Add mongoDB connection

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const app = require('./app')
+const connectDB = require('./config/db')
+
 const dotenv = require('dotenv')
 
 dotenv.config({ path: './config/.env' })
-
+connectDB()
 
 const port = process.env.PORT || 3000
 
