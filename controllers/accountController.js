fa714f5c04edb98f23506ddc3aecd35471350b11
Falola Ibrahim A <ayofalola240@gmail.com>
2021-07-23 16:54:47 +0100

    Add Implement create account in accountController

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,3 +1,7 @@
+const catchAsync = require("./../utils/catchAsync");
+const AppError = require("./../utils/appError")
+const Account = require("./../models/accountModel")
+
 exports.getAllAccounts = (req, res) => {
     res.status(500).json({
         status: 'error',
@@ -10,12 +14,25 @@ exports.getAccount = (req, res) => {
         message: 'This route is yet to be defined!'
     });
 }
-exports.createAccount = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is yet to be defined!'
-    });
-}
+exports.createAccount = catchAsync(async (req, res, next) => {
+    const { username, email, password } = req.body;
+    const accountExist = await Account.findOne({ email });
+
+    if (accountExist) {
+        next(new AppError('account already exists', 400))
+    }
+    const newAccount = await Account.create({
+        username,
+        email,
+        password
+    })
+    res.status(201).json({
+        _id: newAccount._id,
+        name: newAccount.name,
+        email: newAccount.email,
+    })
+
+})
 exports.updateAccount = (req, res) => {
     res.status(500).json({
         status: 'error',
